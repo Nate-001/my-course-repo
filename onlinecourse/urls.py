88dbe0186732bfc0cd88a002105601cce152eeb4
Nate-001 <nathanieldross1@gmail.com>
2024-03-18 18:05:08 -0400

    Changes made to add exam result view and url route.

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('<int:course_id>/submit/', view.submit, name='submit')
 
     # <HINT> Create a route for show_exam_result view
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
